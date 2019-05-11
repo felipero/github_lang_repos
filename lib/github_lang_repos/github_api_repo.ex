fd5332cb3adecf1103c9da5d25882badf5dc6930
Felipe Rodrigues <felipe@crafters.cc>
2019-05-10 23:39:53 -0300

    Changing my access token for github

--- lib/github_lang_repos/github_api_repo.ex
+++ lib/github_lang_repos/github_api_repo.ex
@@ -1,8 +1,7 @@
 defmodule GithubLangRepos.GithubApiRepo do
   def get_language_top_five_repositories(language_name) do
-    client = Tentacat.Client.new(%{access_token: "bd8d280537fa130bee95473931d972db3c1a005d"})
+    client = Tentacat.Client.new(%{access_token: "dbaff2cdc4f6d6a3ce90ce6263347a775c4acdc5"})
     query = [q: "language:#{language_name}", per_page: 5, sort: "stars", order: "desc"]
-    IO.inspect(query)
 
     case Tentacat.Search.repositories(client, query, pagination: :none) do
       {200, response, http_response} ->
